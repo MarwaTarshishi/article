6d274671b356c841be74ca0549d0dbf1c184be04
Marwa Tarshishi <marwatarshishi201@gmail.com>
2025-03-10 13:07:38 +0200

    Update UserSkeleton.php

--- article-server/models/UserSkeleton.php
+++ article-server/models/UserSkeleton.php
@@ -1,14 +1,46 @@
 <?php
-class UserSkeleton {
-    public $id;
-    public $FN; 
-    public $email;
-    public $password;
-    
-    public function __construct($FN = "", $email = "", $password = "") {
-        $this->FN = $FN;
-        $this->email = $email;
-        $this->password = $password;
+
+    class UserSkeleton{
+
+        private $FN;
+        private $email;
+        private $password;
+
+        function __construct($FN, $email, $password) {
+            $this->FN = $FN;
+            $this->set_email($email);
+            $this->password = hash('sha256', $password);
+        }
+        
+        function get_FN() {
+            return $this->FN;
+        }
+
+        function get_email() {
+            return $this->email;
+        }
+        
+        function get_password() {
+            return $this->password;
+        }
+
+        function set_FN($FN) {
+            $this->full_name = $full_name;
+        }
+
+        private function checkEmail($email) {
+            return filter_var($email, FILTER_VALIDATE_EMAIL);
+        }
+
+        function set_email($email) {
+            if($this->checkEmail($email))
+                $this->email = $email;
+            else
+                throw new Exception("Invalid email format");
+        }
+
+        function set_password($password) {
+            $this->password = $password;
+        }
     }
-}
 ?>
