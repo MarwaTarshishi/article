9aecf8995c7fb3fabcf5aa2f75d3bbf3615b96be
Marwa Tarshishi <marwatarshishi201@gmail.com>
2025-03-10 20:42:58 +0200

    Update QuestionSkeleton.php

--- article-server/models/QuestionSkeleton.php
+++ article-server/models/QuestionSkeleton.php
@@ -2,10 +2,12 @@
     class QuestionSkeleton{
         private $question;
         private $answer;
+        private $category;
         
         function __construct($question, $answer) {
             $this->question = $question;
             $this->answer = $answer;
+            $this->category = $category;
         }
         function get_question() {
             return $this->question;
@@ -13,11 +15,17 @@
         function get_answer() {
             return $this->answer;
         }
-        function set_question($question) {
+        function get_category() {
+            return $this->category;
+        }
+          function set_question($question) {
             $this->question = $question;
         }
         function set_answer($answer) {
-            $this->answer = $answer;
+            $this->question = $answer;
+        }
+        function set_category($category) {
+            $this->category = $category;
         }
     }
 ?>
