43fb7da3387bb86ee3c4446f86db645cd4400777
Marwa Tarshishi <marwatarshishi201@gmail.com>
2025-03-10 12:55:56 +0200

    Create connection.php

---
+++ article-server/connection/connection.php
@@ -0,0 +1,24 @@
+<?php
+
+    header('Content-Type: application/json');
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        http_response_code(200);
+        exit();
+    }
+
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $db_name = "article";
+    $port = 3307;
+
+    $conn = new mysqli($host, $user, $password, $db_name, $port);
+
+    if($conn->connect_error)
+        die("connection failed" . mysqli_connect_error());
+
+?>
